e0142e37cd6bde25eb785b82fbe089d3bf56aad0
Kitcat-Alt <streetfion65@gmail.com>
2024-12-17 11:37:44 +0000

    sc

--- SAE/python/Bilan Carbonne/tests_bilan_carbone.py
+++ SAE/python/Bilan Carbonne/tests_bilan_carbone.py
@@ -162,7 +162,6 @@ def test_cumul_temps_activite():
     assert bc.cumul_temps_activite([], bc.co2_minute) == 0
     assert bc.cumul_temps_activite([('Lucas', '2024-09-01', 67.2, 'type3')], bc.co2_minute) == 67.2/0.96
     assert bc.cumul_temps_activite([('Lucas', '2024-09-01', 67.2, 'type3'), ('Lucas', '2024-09-01', 67.2, 'type3')], bc.co2_minute) == 140
-    assert bc.cumul_temps_activite
     
 # ---------------------------------------------------------------------------------------------
 # Ajoutez ici les tests manquants (vos propres fonctions le cas échéant)
