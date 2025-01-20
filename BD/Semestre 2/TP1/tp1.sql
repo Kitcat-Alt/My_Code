be739333c7b6561e30eea1efe67ba0712bc8e901
Kitcat-Alt <streetfion65@gmail.com>
2025-01-20 11:44:54 +0100

    nouv Sql semestre 2

---
+++ BD/Semestre 2/TP1/tp1.sql
@@ -0,0 +1,115 @@
+-- TP 1
+-- Nom:  , Prenom: 
+
+-- +------------------+--
+-- * Question 1 :     --
+-- +------------------+--
+-- Ecrire une requête qui renvoie les informations suivantes:
+--  Donner la liste des panels dont fait partie Caroline BOURIER.
+
+-- Voici le début de ce que vous devez obtenir.
+-- ATTENTION à l'ordre des colonnes et leur nom!
+-- +-----------------+
+-- | nomPan          |
+-- +-----------------+
+-- | France global 1 |
+-- +-----------------+
+-- = Reponse question 1.
+
+
+
+-- +------------------+--
+-- * Question 2 :     --
+-- +------------------+--
+-- Ecrire une requête qui renvoie les informations suivantes:
+--  Quels sont les panels dont un des sondés est de la tranche d'âge 70 à 120 ans?
+
+-- Voici le début de ce que vous devez obtenir.
+-- ATTENTION à l'ordre des colonnes et leur nom!
+-- +-----------------+
+-- | nomPan          |
+-- +-----------------+
+-- | France global 1 |
+-- | France global 2 |
+-- +-----------------+
+-- = Reponse question 2.
+
+
+
+-- +------------------+--
+-- * Question 3 :     --
+-- +------------------+--
+-- Ecrire une requête qui renvoie les informations suivantes:
+--  Quels sont les sondés de la tranche d'age 70-120 ans qui sont ouvriers?
+
+-- Voici le début de ce que vous devez obtenir.
+-- ATTENTION à l'ordre des colonnes et leur nom!
+-- +-----------+------------+
+-- | nomSond   | prenomSond |
+-- +-----------+------------+
+-- | ERYS      | Imane      |
+-- | BERRGAIES | Claire     |
+-- | JABAT     | Rose       |
+-- | WALLOCHE  | Marion     |
+-- | LENUJA    | Pauline    |
+-- | etc...
+-- = Reponse question 3.
+
+
+
+-- +------------------+--
+-- * Question 4 :     --
+-- +------------------+--
+-- Ecrire une requête qui renvoie les informations suivantes:
+--  Quels sont les ouvriers qui portent le prénom Olivier?
+
+-- Voici le début de ce que vous devez obtenir.
+-- ATTENTION à l'ordre des colonnes et leur nom!
+-- +-----------+------------+
+-- | nomSond   | prenomSond |
+-- +-----------+------------+
+-- | THALOUERD | Olivier    |
+-- | POTRININ  | Olivier    |
+-- +-----------+------------+
+-- = Reponse question 4.
+
+
+
+-- +------------------+--
+-- * Question 5 :     --
+-- +------------------+--
+-- Ecrire une requête qui renvoie les informations suivantes:
+--  Quels sont les tranches d'âge qui comportent une ou plusieurs femmes nées un 25 avril?
+
+-- Voici le début de ce que vous devez obtenir.
+-- ATTENTION à l'ordre des colonnes et leur nom!
+-- +----------+--------+
+-- | valDebut | valFin |
+-- +----------+--------+
+-- | 40       | 49     |
+-- +----------+--------+
+-- = Reponse question 5.
+
+
+
+-- +------------------+--
+-- * Question 6 :     --
+-- +------------------+--
+-- Ecrire une requête qui renvoie les informations suivantes:
+--  Quels sont les sondés prénommés Jean qui appartiennent à au moins 2 panels différents? 
+
+-- Voici le début de ce que vous devez obtenir.
+-- ATTENTION à l'ordre des colonnes et leur nom!
+-- +------------+----------+
+-- | prenomSond | nomSond  |
+-- +------------+----------+
+-- | Jean       | DILY     |
+-- | Jean       | JATECHU  |
+-- | Jean       | PIETIENE |
+-- | Jean       | FAL      |
+-- | Jean       | BOYEGHE  |
+-- +------------+----------+
+-- = Reponse question 6.
+
+
+
