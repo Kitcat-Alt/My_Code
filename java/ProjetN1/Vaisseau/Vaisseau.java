f86f6ffccbe4d4aa472f0af01c98323a0f74034c
Kitcat-Alt <streetfion65@gmail.com>
2025-01-21 16:41:34 +0100

    olalalala la doc

---
+++ java/ProjetN1/Vaisseau/Vaisseau.java
@@ -0,0 +1,27 @@
+public class Vaisseau{
+    public Vaisseau(String nom, int puissance, int passage){
+        
+    }
+
+    public Vaisseau(String nom, int puissance){
+
+    }
+
+    public String getNom(){
+        return "";
+    }
+
+
+    public int getNombrePassagers(){
+        return 0;
+    }
+
+    public int getPuissance(){
+        return 0;
+    }
+
+    public boolean transportePassagers(){
+        return false;
+    }
+
+}
