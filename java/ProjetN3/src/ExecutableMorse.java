2ba0ba3887728423e45aebac191e1a8dd8b0494f
Kitcat-Alt <streetfion65@gmail.com>
2025-02-11 23:13:26 +0000

    l'out of range qui me laisse perplexe sur le code que j'ai écrit

--- java/ProjetN3/src/ExecutableMorse.java
+++ java/ProjetN3/src/ExecutableMorse.java
@@ -1,14 +1,17 @@
 public class ExecutableMorse {
     public static void main(String[] Args){
         Lettre n = new Lettre('N');
-        assert n.toChar () == 'N';
-        assert n.toMorse ().equals("===_=");
+        Texte phrase = new Texte("caca comme dirait Magomed");
+        assert n.toChar() == 'N';
+        assert n.toMorse().equals("===_=");
 
         Lettre a = new Lettre("=_===");
-        assert a.toMorse ().equals("=_===");
-        assert a.toChar () == 'A';
+        assert a.toMorse().equals("=_===");
+        assert a.toChar() == 'A';
 
         assert n.toNumero() == 13;
 
+        System.out.println(phrase.toString());
+        System.out.println(phrase.toMorse());
     }
 }
--- java/ProjetN3/src/Texte.java
+++ java/ProjetN3/src/Texte.java
@@ -6,8 +6,25 @@ public class Texte {
     private List<Lettre> ListeLettre;
     
     public Texte (String lettre){
+        this.ListeLettre = new ArrayList<>();
         for(int i=0; i<lettre.length(); i++){
             this.ListeLettre.add(new Lettre(lettre.charAt(i)));
         }
+    }
+
+    public String toString(){
+        String chaine = "";
+        for(int i=0; i<this.ListeLettre.size(); i++){
+            chaine = chaine + this.ListeLettre.get(i).toChar();
+        }
+        return chaine;
+    }
+
+    public String toMorse(){
+        String chaine = "";
+        for(int i=0; i<this.ListeLettre.size(); i++){
+            chaine = chaine + this.ListeLettre.get(i).toMorse();
+        }
+        return chaine;
     }
 }  
